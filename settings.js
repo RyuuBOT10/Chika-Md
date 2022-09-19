dad9397acb489f9cad445b720dfa3bb402754c89
RyuuBOT10 <113424441+RyuuBOT10@users.noreply.github.com>
2022-09-19 17:48:18 +0700

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 /*
-› Create By @AkenoSTOREID
-› Base Ori @Ryuu
+› Create By @RIY
+› Base Ori @DikaArdnt
 › Kalau mau tambahin fitur di index.js
 › Edit text menu di file language › help.js
 */
@@ -18,31 +18,31 @@ zenz: 'https://zenzapis.xyz',
 //—————「 Website Apikey 」—————//
 global.APIKeys = {
 //gausah di apa² in, udah free apikey :)
-'https://zenzapis.xyz': 'BagasGanz',
+'https://zenzapis.xyz': 'BagasPrdn',
 }
 
 //—————「 Setting Owner 」—————//
-global.owner = ['6289524117886','6289524117886','6289524117886'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.premium = ['6289524117886'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-global.ownernomer = '6289524117886' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Own & Bot 」—————//
-global.ownername = 'AkenoSTOREID' //ubah jadi nama mu, note tanda ' gausah di hapus!
-global.botname = 'AkenoBOTQ' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.footer = 'AkenoID' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.ownername = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.footer = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Kebutuhan Button & Kontak 」—————//
 //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-global.youtube = ''
-global.ig = ''
-global.mygc = 'https://chat.whatsapp.com/CAysLiwZvSE44LEv1i7nNm'
-global.myweb = ''
-global.email = ''
+global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
+global.ig = 'https://instagram.com/riychdwayne'
+global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
+global.myweb = 'https://s.id/RiyAPIs'
+global.email = 'riychdwaynee@gmail.com'
 global.region = 'indonesia'
 
 //—————「 Set Wm 」—————//
-global.packname = '© AkenoBOT1' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-global.author = 'AkenoID' //ubah jadi nama mu, note tanda ' gausah di hapus!
+global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+global.author = 'RIY' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
 //—————「 Set Nama Session 」—————//
 //gausah di apa² in!
@@ -58,7 +58,7 @@ free: 15,
 //—————「 Set Image 」—————//
 //terserah mau ubah apa nggak, menurut gua jangan di ubah!
 global.thumb = fs.readFileSync('./image/chika.jpg')
-/*global.veloriy = fs.readFileSync('./image/chika.mp4')*/
+global.veloriy = fs.readFileSync('./image/chika.mp4')
 
 //—————「 Set Image Welcome 」—————//
 //terserah mau ubah apa nggak, menurut gua jangan di ubah!
